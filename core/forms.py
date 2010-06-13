af76240698b63f826469df65f6f069c5fa97780a
Eric Martin <e.a.martin1337@gmail.com>
2010-06-13 05:52:25 +0000

    Removed core

--- core/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class RegForm(forms.Form):
-	email = forms.EmailField() #add validators for email and username
-	email_again = forms.EmailField()
-	username = forms.CharField(max_length=20)
-	password = forms.CharField(max_length=30, widget = forms.PasswordInput)
-	password_again = forms.CharField(max_length=30, widget = forms.PasswordInput)
