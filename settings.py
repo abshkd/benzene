58f7a4c0fb938b0f54d6927bd26a903090553728
Samuel Baldwin <recursive.forest@gmail.com>
2010-07-12 14:38:11 -0400

    Update settings.py for database changes, remove pyc files.

--- settings.py
+++ settings.py
@@ -19,8 +19,8 @@ DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 		'NAME': 'benzene',					  # Or path to database file if using sqlite3.
-		'USER': 'eric',					  # Not used with sqlite3.
-		'PASSWORD': 'abc123',				  # Not used with sqlite3.
+		'USER': 'benzene_user',					  # Not used with sqlite3.
+		'PASSWORD': '',				  # Not used with sqlite3.
 		'HOST': '',					  # Set to empty string for localhost. Not used with sqlite3.
 		'PORT': '',					  # Set to empty string for default. Not used with sqlite3.
 	}
