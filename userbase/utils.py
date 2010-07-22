5927b29fe7e10aed1399cbb770b42ef83cea2778
Samuel Baldwin <recursive.forest@gmail.com>
2010-07-22 15:34:04 -0400

    Fix whitespace issue.

--- userbase/utils.py
+++ userbase/utils.py
@@ -24,17 +24,17 @@ def create_password(raw_pass):
 	return '%s$%s$%s' % (algo, salt, hsh)
 	
 def login_user(request, user):
-    """
-    Log in a user without requiring credentials (using ``login`` from
-    ``django.contrib.auth``, first finding a matching backend).
+	"""
+	Log in a user without requiring credentials (using ``login`` from
+	``django.contrib.auth``, first finding a matching backend).
 	Source is http://djangosnippets.org/snippets/1547/
-    """
-    from django.contrib.auth import load_backend, login
-    import settings
-    if not hasattr(user, 'backend'):
-        for backend in settings.AUTHENTICATION_BACKENDS:
-            if user == load_backend(backend).get_user(user.pk):
-                user.backend = backend
-                break
-    if hasattr(user, 'backend'):
-        return login(request, user)
+	"""
+	from django.contrib.auth import load_backend, login
+	import settings
+	if not hasattr(user, 'backend'):
+		for backend in settings.AUTHENTICATION_BACKENDS:
+			if user == load_backend(backend).get_user(user.pk):
+				user.backend = backend
+				break
+	if hasattr(user, 'backend'):
+		return login(request, user)
