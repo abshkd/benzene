141b7fc5d5d5a01f798d76e5663ae48c04c4159d
Eric Martin <e.a.martin1337@gmail.com>
2010-07-22 22:50:59 +0000

    Added private_messages to installed_apps

--- search_sites.py
+++ search_sites.py
@@ -3,12 +3,13 @@ from haystack.indexes import RealTimeSearchIndex
 from queued_search.indexes import QueuedSearchIndex
 from settings import DEBUG
 
-haystack.autodiscover()
-
 def get_index():
 	'''All search indexes for models should call this function as their super class.'''
 	
 	if DEBUG:
 		return RealTimeSearchIndex
 	return QueuedSearchIndex
+	
+haystack.autodiscover()
+
 
